2fa38b082d96f5ca4b07b8068a0fbb567b036ebb
Batmaaannn <stainvy@gmail.com>
2025-06-26 19:56:59 +0200

    feat: add API tags to AuthController and enhance JWT guard for public route handling

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -19,6 +19,7 @@ export class AuthService {
   ) {}
 
   async validateUser(email: string, password: string): Promise<any> {
+    console.log("AuthService: validateUser called with email:", email);
     const user = await this.usersService.findOneByEmail(email);
 
     if (!user)
