1da61707b3ba460a057d8c3813a7a42010ea5353
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-09-01 00:09:34 +0200

    Ajout d'une méthode permettant de récupérer des entrepôts.

--- src/modules/warehouses/warehouses.controller.ts
+++ src/modules/warehouses/warehouses.controller.ts
@@ -1,4 +1,19 @@
 import { Controller } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Warehouse } from './entities/warehouse.entity';
+import { Repository } from 'typeorm';
 
 @Controller('warehouses')
-export class WarehousesController {}
+export class WarehousesController {
+  constructor(
+    @InjectRepository(Warehouse)
+    private readonly warhousesRepository: Repository<Warehouse>
+  ) {}
+
+  /**
+   * Permet de récupérer les entrepôts.
+   */
+  async getWarehouses(): Promise<Warehouse[]> {
+    return await this.warhousesRepository.find();
+  }
+}
