d88b23bc3cece11279ce7903976ba4b232ae46c5
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-08-31 21:21:44 +0200

    Écriture du controller StoragesController.

--- src/modules/storages/storages.controller.ts
+++ src/modules/storages/storages.controller.ts
@@ -1,4 +1,59 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { CustomerStorageBox } from './entities/customer-storage-box.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { WarehouseStorage } from './entities/warehouse-storage.entity';
+import { BoxSize } from 'src/types/box';
+import { StoragesService } from './storages.service';
+
+type BoxType = {
+  warehouseId: number;
+  size?: BoxSize;
+  rentalStart: Date;
+  rentalEnd: Date;
+}
+
+type RentalType = {
+  boxId: number;
+  additionalDays: number;
+}
 
 @Controller('storages')
-export class StoragesController {}
+export class StoragesController {
+  constructor(
+    private readonly storageService: StoragesService,
+    @InjectRepository(CustomerStorageBox)
+    private readonly customerStorageBoxRepository: Repository<CustomerStorageBox>,
+    @InjectRepository(WarehouseStorage)
+    private readonly warehouseStorageRepository: Repository<WarehouseStorage>,
+  ) { }
+
+  /**
+   * Permet de récupérer toutes les boxs disponibles.
+   */
+  @Get("/get-available-boxes")
+  async getAvailableBoxes({ warehouseId, size, rentalStart, rentalEnd }: BoxType) {
+    return this.storageService.findAvailableBoxes({ warehouseId, size, rentalStart, rentalEnd });
+  }
+
+  /**
+   * Créé une réservation de box.
+   */
+  @Post("/create-reservation-box")
+  async createReservationBox({ warehouseId, size, rentalStart, rentalEnd }: BoxType) {
+    const foundWarehouse = await this.warehouseStorageRepository.findOneBy({
+      id: warehouseId
+    });
+
+    this.customerStorageBoxRepository.create({
+      warehouse: foundWarehouse,
+      box_size: size,
+      rental_start: rentalStart,
+      rental_end: rentalEnd
+    });
+  }
+
+  async extendRental({ boxId, additionalDays }: RentalType) {
+    // 
+  }
+}
